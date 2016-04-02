9f4dfbffbf80de823de02b5e7b9294577b04261a
Dmytro Udielov <Dmytro Udielov>
2016-04-02 18:21:22 +0300

    Softheme School A_1

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Algorithm_1
+{
+    class Program
+    {
+        public static Random rnd = new Random();
+        const string filePathInput = "Input.txt";
+        const string filePathOutput = "Output.txt";
+
+
+        static void Main(string[] args)
+        {
+            CreateAndInitializate();
+            OutputResult();
+
+        }
+
+        //Input.txt create\generate
+        public static void CreateAndInitializate()
+        {
+            
+            const int maxLength = 100;
+
+            try
+            {
+                if (!File.Exists(filePathInput))
+                {
+                    File.Create(filePathInput);
+                }
+                else
+                {
+                    string repo = null;
+
+                    for (int i = 0; i <= maxLength; i++)
+                    {
+                        repo += rnd.Next(0, 2).ToString();
+                    }
+
+                    File.WriteAllText(filePathInput, repo);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        //Output
+        public static void OutputResult()
+        {
+            try
+            {
+                string text = File.ReadAllText(filePathInput);
+                var result = text.Split('0');
+
+                if (!File.Exists(filePathOutput))
+                {
+                    File.Create(filePathOutput);
+                }
+                else
+                {
+                    File.WriteAllText(filePathOutput, result.Max().Length.ToString());
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
